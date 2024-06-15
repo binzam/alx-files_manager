bf13674c898663e035c4d5b3c4cf52b539e2935a
binzam <btechan@gmail.com>
2024-06-16 02:07:35 +0300

    task 0

---
+++ utils/redis.js
@@ -0,0 +1,59 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.connected = true;
+    this.client.on('error', (err) => {
+      console.error('Redis client error:', err.toString());
+      this.client.connected = false;
+    });
+    this.client.on('connect', () => {
+      this.client.connected = true;
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
