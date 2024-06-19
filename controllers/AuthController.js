0af411a1b7cd80ea29db3a39917fdd34dc744f5e
binzam <btechan@gmail.com>
2024-06-19 21:28:39 +0300

    idk

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,19 +1,36 @@
 import { v4 as uuidv4 } from 'uuid';
+import sha1 from 'sha1';
 import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AuthController {
   static async getConnect(req, res) {
     try {
-      const { user } = req;
-      const accessToken = uuidv4();
+      const authHeader = req.headers.authorization;
+      if (!authHeader || !authHeader.startsWith('Basic ')) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const base64Credentials = authHeader.split(' ')[1];
+      const credentials = Buffer.from(base64Credentials, 'base64').toString(
+        'utf-8',
+      );
+      const [email, password] = credentials.split(':');
+      const user = await dbClient.findUserByEmail(email);
       if (!user) {
         return res.status(401).json({ error: 'Unauthorized' });
       }
+      if (user.password !== sha1(password)) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+      const accessToken = uuidv4();
+
       await redisClient.set(
         `auth_${accessToken}`,
         user._id.toString('utf8'),
         86400,
       );
+
       return res.status(200).json({ token: accessToken });
     } catch (error) {
       console.error('Error in getConnect:', error);
@@ -27,6 +44,14 @@ class AuthController {
     try {
       const token = req.headers['x-token'];
       if (!token) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+      const userId = await redisClient.get(`auth_${token}`);
+      if (!userId) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+      const user = await dbClient.findUserById(userId);
+      if (!user) {
         return res.status(401).json({ error: 'Unauthorized' });
       }
       await redisClient.del(`auth_${token}`);
--- utils/db.js
+++ utils/db.js
@@ -1,19 +1,17 @@
 import { MongoClient, ObjectId } from 'mongodb';
 import sha1 from 'sha1';
 
-const HOST = process.env.DB_HOST || 'localhost';
-const PORT = process.env.DB_PORT || 27017;
-const DATABASE = process.env.DB_DATABASE || 'files_manager';
-const url = `mongodb://${HOST}:${PORT}`;
-
 class DBClient {
   constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
     this.connected = false;
     this.client = new MongoClient(url);
     this.client
       .connect()
       .then(() => {
-        this.db = this.client.db(`${DATABASE}`);
         this.connected = true;
       })
       .catch((err) => console.log(err.message));
@@ -25,7 +23,8 @@ class DBClient {
 
   async nbUsers() {
     try {
-      const usersCollection = this.db.collection('users');
+      const db = this.client.db(this.database);
+      const usersCollection = db.collection('users');
       const count = await usersCollection.countDocuments();
       return count;
     } catch (error) {
@@ -36,7 +35,8 @@ class DBClient {
 
   async nbFiles() {
     try {
-      const filesCollection = this.db.collection('files');
+      const db = this.client.db(this.database);
+      const filesCollection = db.collection('files');
       const count = await filesCollection.countDocuments();
       return count;
     } catch (error) {
@@ -46,7 +46,8 @@ class DBClient {
   }
 
   async userExist(email) {
-    const usersCollection = this.db.collection('users');
+    const db = this.client.db(this.database);
+    const usersCollection = db.collection('users');
     const user = await usersCollection.findOne({ email });
     if (user) {
       return true;
@@ -55,7 +56,8 @@ class DBClient {
   }
 
   async createUser(email, password) {
-    const usersCollection = this.db.collection('users');
+    const db = this.client.db(this.database);
+    const usersCollection = db.collection('users');
     const hashedPwd = sha1(password);
     const user = await usersCollection.insertOne({
       email,
@@ -67,58 +69,25 @@ class DBClient {
   async findUserById(id) {
     try {
       const _id = new ObjectId(id);
-      const usersCollection = this.db.collection('users');
+      const db = this.client.db(this.database);
+      const usersCollection = db.collection('users');
       const user = await usersCollection.findOne({ _id });
       return user || null;
     } catch (error) {
-      console.error('Error in getUserById:', error);
+      console.error('Error in getUserByToken:', error);
       throw new Error('An error occurred while retrieving the user');
     }
   }
 
   async findUserByEmail(email) {
     try {
-      const usersCollection = this.db.collection('users');
+      const db = this.client.db(this.database);
+      const usersCollection = db.collection('users');
       const user = await usersCollection.findOne({ email });
       return user || null;
     } catch (error) {
       console.error('Error in findUserByEmail:', error);
       throw new Error('An error occurred while retrieving the user');
-    }
-  }
-
-  async saveFileInDb(fileDocument) {
-    try {
-      console.log('filedoc', fileDocument);
-      const filesCollection = this.db.collection('files');
-      const result = await filesCollection.insertOne({
-        userId: fileDocument.userId,
-        name: fileDocument.name,
-        type: fileDocument.type,
-        isPublic: fileDocument.isPublic,
-        parentId: fileDocument.parentId || 0,
-        localPath: fileDocument.localPath,
-      });
-      if (!result.acknowledged) {
-        return null;
-      }
-      return result;
-    } catch (error) {
-      console.error('Error saving file in database:', error);
-      throw error;
-    }
-  }
-
-  async getFileFromDb(fileId) {
-    try {
-      const idObject = new ObjectId(fileId);
-      console.log(idObject);
-      const filesCollection = this.db.collection('files');
-      const file = await filesCollection.findOne({ _id: idObject });
-      return file;
-    } catch (error) {
-      console.error('Error in getFilefromDb:', error);
-      throw error;
     }
   }
 }
